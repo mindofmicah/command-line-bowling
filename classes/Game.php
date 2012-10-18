97d20e594878633cf0b0a511aa96ebd5d81ac806
WEB3 <micah@ubuntu.ubuntu-domain>
2012-10-18 13:02:53 -0500

    Added in a method to display the scoreboard that loops through the frames

--- classes/Game.php
+++ classes/Game.php
@@ -4,8 +4,30 @@ class Game
 	protected $frames = array();
 	public function __construct()
 	{
-		for ($i = 0; $i <=10; $i++) {
+		for ($i = 1; $i <=10; $i++) {
 			$this->frames[] = Frame::factory($i);
 		}
+	}
+
+	public function displayScoreBoard() {
+
+		$lines = array();
+		foreach ($this->frames as $frame) {
+
+			$rows = $frame->toDisplayRows();
+			foreach ($rows as $index => $row) {
+				$lines[$index][] = $row;
+			}
+		}
+		
+		$ret = '';
+		for ($i = 0, $numLines = count($lines); $i < $numLines; $i++) {
+			$ret.=  ($i == 0 || $i == $numLines-1?'+':'|')
+				.   implode(($i == 0 || $i == $numLines-1?'+':'|'),$lines[$i]) .  
+  ($i == 0 || $i == $numLines-1?'+':'|')
+
+				    . "\n";
+		}
+		return $ret;
 	}
 }
