32d9b078bd7dd117ebb4b9dd631d71918734f7b1
micah@work <micah@ubuntu.ubuntu-domain>
2012-10-18 13:37:31 -0500

    Added the necessary addRoll methods

--- classes/Frame/Normal.php
+++ classes/Frame/Normal.php
@@ -1,6 +1,10 @@
 <?php
 class Frame_Normal extends Frame
 {
+	public function addRoll($number)
+	{
+		echo 'trying to add ' . $number . " to a normal frame ($this->number)\n";
+	}
 	public function __construct($number)
 	{
 		$this->number = $number;
--- classes/Frame/Tenth.php
+++ classes/Frame/Tenth.php
@@ -1,6 +1,10 @@
 <?php
 class Frame_Tenth extends Frame
 {
+	public function addRoll($number)
+	{
+		echo "tried adding $number to the tenth frame";
+	}
 	public function __construct()
 	{
 		$this->number = 10;
