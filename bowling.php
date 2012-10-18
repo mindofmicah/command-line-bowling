1d16958af4b2ff678ba5de10d99c72eb09bbd4e3
micah@work <michaeleschbacher@gmail.com>
2012-10-18 15:39:05 -0500

    Added a loop to keep the game going until the frames are all full

--- bowling.php
+++ bowling.php
@@ -2,10 +2,9 @@
 require 'autoload.php';
 
 $game = new Game();
-$game->addRoll(4);
-$game->addRoll(5);
-$game->addRoll(1);
+
+while ($game->getActive()) {
+	$game->addRoll(4);
+}
 
 echo $game->displayScoreBoard();
-
-#print_r($game);
