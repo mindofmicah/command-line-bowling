a5253078dcb3a1ee6bb038d8ddf6902bdbaa5dab
micah@work <michaeleschbacher@gmail.com>
2012-10-18 14:38:11 -0500

    Added logic for processing the new roll to a frame

--- classes/Frame/Normal.php
+++ classes/Frame/Normal.php
@@ -1,8 +1,20 @@
 <?php
 class Frame_Normal extends Frame
 {
+	const MAX_ROLLS = 2;
+
 	public function addRoll($number)
 	{
+		if(count($this->rolls) == self::MAX_ROLLS) {
+			throw new Exception('Cannot and roll');
+		}
+
+		$this->rolls[] = $number;
+
+		if(count($this->rolls) == self::MAX_ROLLS) {
+			$this->isFull = true;
+		}
+
 		echo 'trying to add ' . $number . " to a normal frame ($this->number)\n";
 	}
 	public function __construct($number)
