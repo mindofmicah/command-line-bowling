50a771759a0af1ff7213d215ccf4fa4297b05a59
micah@work <micah@ubuntu.ubuntu-domain>
2012-10-18 13:35:06 -0500

    Added some hardcoded rolls to add

--- bowling.php
+++ bowling.php
@@ -2,6 +2,12 @@
 require 'autoload.php';
 
 $game = new Game();
+$game->addRoll(4);
+$game->addRoll('X');
+$game->addRoll(42);
+$game->addRoll('fasd');
+$game->addRoll(6);
+
 
 echo $game->displayScoreBoard();
 
